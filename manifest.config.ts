fccc2d0ee88b1b15ff2c7c0200401d1166ff659b
Quang <nmquang2012@gmail.com>
2026-04-01 13:26:47 +0700

    feat(manifest): update logo files and remove old logo

--- manifest.config.ts
+++ manifest.config.ts
@@ -1,23 +1,24 @@
-import { defineManifest } from '@crxjs/vite-plugin'
-import pkg from './package.json'
+import { defineManifest } from '@crxjs/vite-plugin';
+import pkg from './package.json';
 
 export default defineManifest({
   manifest_version: 3,
   name: pkg.name,
   version: pkg.version,
   icons: {
-    48: 'public/logo.png',
+    16: 'public/logo-16.png',
+    48: 'public/logo-48.png',
+    128: 'public/logo-128.png',
   },
   action: {
     default_icon: {
-      48: 'public/logo.png',
+      16: 'public/logo-16.png',
+      48: 'public/logo-48.png',
+      128: 'public/logo-128.png',
     },
     default_popup: 'src/popup/index.html',
   },
-  permissions: [
-    'sidePanel',
-    'contentSettings',
-  ],
+  permissions: ['sidePanel', 'contentSettings'],
   // content_scripts: [{
   //   js: ['src/content/main.tsx'],
   //   matches: ['https://*/*'],
@@ -25,4 +26,4 @@ export default defineManifest({
   // side_panel: {
   //   default_path: 'src/sidepanel/index.html',
   // },
-})
+});
