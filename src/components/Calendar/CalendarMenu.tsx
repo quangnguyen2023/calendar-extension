0d9cce7f413eb7278e1b470f04bb3d6bbb9f11e6
Quang <nmquang2012@gmail.com>
2026-04-01 14:54:53 +0700

    feat(calendar): enhance CalendarMenu with loading indicator and adjust MonthNavigator styles

--- src/components/Calendar/CalendarMenu.tsx
+++ src/components/Calendar/CalendarMenu.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense, useState } from 'react';
-import { EllipsisVertical } from 'lucide-react';
+import { EllipsisVertical, LoaderIcon } from 'lucide-react';
 
 import {
   Popover,
@@ -19,7 +19,7 @@ export default function CalendarMenu() {
   return (
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
-        <Button variant="ghost" size="icon">
+        <Button variant="ghost" size="icon-sm" className="rounded-full">
           <EllipsisVertical width={18} height={18} />
         </Button>
       </PopoverTrigger>
@@ -32,7 +32,7 @@ export default function CalendarMenu() {
         </PopoverHeader>
 
         <div className="pt-4 w-70">
-          <Suspense fallback={null}>
+          <Suspense fallback={<LoaderIcon className="mx-auto my-4 size-5 animate-spin" />}>
             <QuickViewByDate onClose={() => setOpen(false)} />
           </Suspense>
         </div>
